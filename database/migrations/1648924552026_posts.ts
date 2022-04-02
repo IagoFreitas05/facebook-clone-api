d191a38035d304fc7302b31c02240b46b7b52b9f
Iago Freitas <iagofreitas05@gmail.com>
2022-04-02 16:33:12 -0300

    posts

--- database/migrations/1648924552026_posts.ts
+++ database/migrations/1648924552026_posts.ts
@@ -7,10 +7,9 @@ export default class Posts extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
       table.text("description", "longtext");
-      table.integer("user_id")
-        .unsigned()
+      table.integer("user_id").unsigned()
         .references("users.id")
-        .onUpdate("CASCATE")
+        .onUpdate("CASCADE")
         .onDelete("CASCADE");
 
 
